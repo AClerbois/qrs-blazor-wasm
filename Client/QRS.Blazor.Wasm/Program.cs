4170e9b864f15d2c54c13f53d2c27ec2fbcce237
Adrien Clerbois <adrien@senseof.tech>
2021-06-23 11:46:17 +0200

    Change version 5.0

--- Client/QRS.Blazor.Wasm/Program.cs
+++ Client/QRS.Blazor.Wasm/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace QRS.Blazor.WebAssembly
+namespace QRS.Blazor.Wasm
 {
     public class Program
     {
